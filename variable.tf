6c1733816037632008323052d905d30a5d950c88
jagadisdevops <134026862+jagadisdevops@users.noreply.github.com>
2023-08-08 07:32:51 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,11 @@
 provider "aws" {
-        region          ="us-east-1"
-        access_key              ="AKIAZWQNTFDD6Y7DS64D"
-        secret_key              ="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"
-        }
+	region = "ap-south-1"
+	access_key = "AKIAUVGNFZFGZWCLJTOW"
+	secret_key     = "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"
+}
+variable "name" {
+  default = "rajartechdev"
+}
+variable "name" {
+  default = "rajartechdev"
+}
